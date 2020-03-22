0b427a9ab288d5111cc4da0c359d8dc57ca4a80f
Gabriel Lima <gabriel.luz.lima@hotmail.com>
2020-03-22 12:27:21 -0300

    modificacoes de estilo de codigo

--- core.py
+++ core.py
@@ -1,72 +1,76 @@
-from sqlalchemy import (create_engine, MetaData, Column,
+'''operacoes do banco de dados'''
+from sqlalchemy import (create_ENGINE, MetaData, Column,
                         Table, Integer, String, select, update, ForeignKey)
-import json
 
-engine = create_engine('sqlite:///teste.db', echo=False)
+ENGINE = create_ENGINE('sqlite:///TESTE.db', echo=False)
 
-metadata = MetaData(bind=engine)
+METADATA = MetaData(bind=ENGINE)
 
-users = Table('usuarios', metadata,
+USERS = Table('usuarios', METADATA,
               Column('id', Integer, primary_key=True),
               Column('nome', String(40), index=True),
               Column('senha', String),
-              Column('numero_de_serie', String,  nullable=False))
-cafeteira_status = Table('Cafeteira_status', metadata,
+              Column('numero_de_serie', String, nullable=False))
+CAFETEIRA_STATUS = Table('Cafeteira_status', METADATA,
                          Column('agua_disponivel', Integer),
                          Column('cafe_disponivel', Integer),
                          Column('n_de_serie', String,
-                                ForeignKey(users.c.numero_de_serie)))
-cafeteira_tasks = Table('Cafeteira_tasks', metadata,
+                                ForeignKey(USERS.c.numero_de_serie)))
+CAFETEIRA_TASKS = Table('Cafeteira_tasks', METADATA,
                         Column('id', Integer, primary_key=True),
                         Column('cafe', Integer, nullable=False),
                         Column('agua', Integer, nullable=False),
                         Column('horario', String, nullable=False),
                         Column('n_de_serie', String,
-                               ForeignKey(users.c.numero_de_serie)))
-teste = Table('horario',metadata,
-               Column('id', Integer, primary_key=True),
-               Column('hora',String))
+                               ForeignKey(USERS.c.numero_de_serie)))
+TESTE = Table('horario', METADATA,
+              Column('id', Integer, primary_key=True),
+              Column('hora', String))
 
-# metadata.create_all()
+# METADATA.create_all()
 
 def Update(status):
-    stmt = update(cafeteira_status).where(cafeteira_status.c.n_de_serie==status['n_de_serie']).values(cafe_disponivel=status['cafe_disponivel']).execute()
+    '''Opercao de update'''
+    stmt = update(CAFETEIRA_STATUS).where(CAFETEIRA_STATUS.c.n_de_serie == status['n_de_serie']).values(cafe_disponivel=status['cafe_disponivel']).execute()
     return True
 
 
 def insert_status(status):
-    conn = engine.connect()
-    status['n_de_serie'] = select([users.c.numero_de_serie]).where(users.c.nome == status['nome'])
-    cafeteira = cafeteira_status.insert()
-    Status = cafeteira.values(cafe_disponivel=status['cafe_disponivel'],
+    '''opercao insert'''
+    conn = ENGINE.connect()
+    status['n_de_serie'] = select([USERS.c.numero_de_serie]).where(USERS.c.nome == status['nome'])
+    cafeteira = CAFETEIRA_STATUS.insert()
+    status = cafeteira.values(cafe_disponivel=status['cafe_disponivel'],
                               agua_disponivel=status['agua_disponivel'],
                               n_de_serie=status['n_de_serie'])
     print(status)
-    conn.execute(Status)
+    conn.execute(status)
     conn.close()
     return True
 
 
 def insert_task(settings):
-    n_de_serie = select([users.c.numero_de_serie]).where(users.c.nome == settings['nome'])
+    '''opercao insert'''
+    n_de_serie = select([USERS.c.numero_de_serie]).where(USERS.c.nome == settings['nome'])
     for row in n_de_serie.execute():
         print(row)
-        settings['n_de_serie']=row[0]
-    conn = engine.connect()
-    cafeteira = cafeteira_tasks.insert()
+        settings['n_de_serie'] = row[0]
+    conn = ENGINE.connect()
+    cafeteira = CAFETEIRA_TASKS.insert()
     new_task = cafeteira.values(cafe=settings['cafe'],
                                 agua=settings['agua'],
                                 horario=settings['horario'],
                                 n_de_serie=settings['n_de_serie'])
     print(settings)
     conn.execute(new_task)
-    conn.close
+    conn.close()
     return True
 
 
 def insert_user(user):
-    conn = engine.connect()
-    user_ins = users.insert()
+    '''opercao insert'''
+    conn = ENGINE.connect()
+    user_ins = USERS.insert()
     new_user = user_ins.values(nome=user['name'], senha=user['password'],
                                numero_de_serie=user['n_de_serie'])
     print(user)
@@ -76,46 +80,51 @@ def insert_user(user):
 
 
 def autenticate(nome, senha):
-    user = select([users.c.senha]).where(users.c.nome == nome)
+    '''select pra comparacao de login'''
+    user = select([USERS.c.senha]).where(USERS.c.nome == nome)
     for x in user.execute():
         if x[0] == senha:
             return True
         else:
             return False
 def get_task(N_de_serie):
-    data = select([cafeteira_tasks.c.id,
-                   cafeteira_tasks.c.agua,
-                   cafeteira_tasks.c.cafe,
-                   cafeteira_tasks.c.horario,
-                   cafeteira_tasks.c.n_de_serie]).where(cafeteira_tasks.c.n_de_serie == N_de_serie).execute()
-    task =  {_id:{'id': _id,"cafe":cafe,
-                        "agua":agua,
-                        "horario":horario,
-                        "n_de_serie":n_de_serie}
-            for _id,agua,cafe,horario,n_de_serie in data}
+    '''select que retorna tarefas'''
+    data = select([CAFETEIRA_TASKS.c.id,
+                   CAFETEIRA_TASKS.c.agua,
+                   CAFETEIRA_TASKS.c.cafe,
+                   CAFETEIRA_TASKS.c.horario,
+                   CAFETEIRA_TASKS.c.n_de_serie]).where(CAFETEIRA_TASKS.c.n_de_serie == N_de_serie).execute()
+    task = {_id:{'id': _id, "cafe":cafe,
+                 "agua":agua,
+                 "horario":horario,
+                 "n_de_serie":n_de_serie}
+            for _id, agua, cafe, horario, n_de_serie in data}
     return task
-def insert_teste(Hora):
-    conn = engine.connect()
-    teste_insert= teste.insert()
-    novo_horario = teste_insert.values(hora=Hora)
-    print(Hora)
+def insert_teste(hora):
+    '''opercao insert'''
+    conn = ENGINE.connect()
+    teste_insert = TESTE.insert()
+    novo_horario = teste_insert.values(hora=hora)
+    print(hora)
     conn.execute(novo_horario)
     conn.close()
-    return Hora
+    return hora
 def get_teste():
-    x = select([teste])
-    lista = [ i for i in x.execute()]
+    '''teste'''
+    x = select([TESTE])
+    lista = [i for i in x.execute()]
     return lista
 
 
 def delete(_id):
-    Del = cafeteira_tasks.delete().where(cafeteira_tasks.c.id == _id)
-    Del.execute()
+    '''delete'''
+    dele = CAFETEIRA_TASKS.delete().where(CAFETEIRA_TASKS.c.id == _id)
+    dele.execute()
     return True
 
 
 def get_serial(password):
-    query = select([users.c.numero_de_serie]).where(users.c.senha == password)
+    '''retorna o numero de serie'''
+    query = select([USERS.c.numero_de_serie]).where(USERS.c.senha == password)
     for i in query.execute():
         return i
-
